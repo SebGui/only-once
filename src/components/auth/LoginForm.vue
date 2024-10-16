32c7931fb8628248bed941fff099c46f0483a029
Sebastien <sebastien.guibourdin@gmail.com>
2024-10-16 13:00:04 +0200

    Login page rebuild (Mobile/Tablet)

--- src/components/auth/LoginForm.vue
+++ src/components/auth/LoginForm.vue
@@ -1,5 +1,11 @@
 <template>
-    <div>
+    <div class="loginContainer">
+
+        <!-- Mobile & tab icon -->
+         <div class="loginIcon">
+            <i class="material-icons">input</i>
+         </div>
+
         <form @submit.prevent="handleLogin" id="loginForm">
             <input type="text" placeholder="Enter your login..." v-model="login" required autofocus>
             <input type="password" placeholder="Enter your password..." v-model="password" required>
@@ -7,13 +13,24 @@
             <span v-if="authStore.errorText != null" class="errorText">{{ authStore.errorText }}</span>
             <span v-if="authStore.successText != null" class="successText">{{ authStore.successText }}</span>
 
-            <button>Submit</button>
+            <button>
+                <i class="material-icons">login</i>
+                Log in
+            </button>
         </form>
 
         <div class="switchAuth">
-            <span @click="handleSwitch('register')">Register</span> /
-            <span @click="handleSwitch('forgot')">Forgot password</span>
+            <button @click="handleSwitch('register')">
+                <i class="material-icons">app_registration</i>
+                Register
+            </button>
+
+            <button @click="handleSwitch('forgot')">
+                <i class="material-icons">psychology_alt</i>
+                Forgot password
+            </button>
         </div>
+
     </div>
 </template>
 
@@ -61,6 +78,10 @@
     }
     button {
         margin-top:20px;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
     }
     .switchAuth {
         margin-bottom:10px;
@@ -76,5 +97,73 @@
     }
     .successText {
         color: var(--main-success-color);
+    }
+
+    .loginIcon {
+        display:none;
+        margin: 0px auto;
+    }
+    .loginContainer {background-color: var(--main-bg-color);}
+
+    .switchAuth i, #loginForm i {
+        margin-right:10px;
+        font-size:40px;
+    }
+    #loginForm button {
+        color:white;
+        background-color: var(--main-bg-color-darken);
+    }
+
+    /* Tablet */
+    @media (max-width: 1024px) {
+        .loginContent {}
+        .loginIcon {
+            color:white;
+            display:block;
+        }
+        .loginIcon .material-icons {
+            font-size: 120px;
+        }
+        .loginContainer {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            height: 100%;
+        }
+        #loginForm {
+            width: 100%;
+        }
+        #loginForm input {
+            height:40px;
+            font-size: 16px;
+            padding: 5px;
+        }
+        #loginForm button {
+            width:50%;
+            height:50px;
+            font-size: 16px;
+            margin-top: 70px;
+            margin: 70px auto 20px auto;
+        }
+        .switchAuth {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            width:100%;
+        }
+        .switchAuth button {
+            width:50%;
+            height:50px;
+            font-size: 16px;
+        }
+    }
+    /* Phone */
+    @media (max-width: 768px) {
+        .loginContent {}
+        .loginIcon {
+            display:block;
+        }
     }
 </style>
--- src/views/AuthView.vue
+++ src/views/AuthView.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
-        <h1>Auth page</h1>
+        <!--<h1>Auth page</h1>-->
 
-        <div class="borders">
+        <div class="borders loginContent">
             <LoginForm v-if="showForm === 'login'" />
             <RegisterForm v-if="showForm === 'register'"/>
             <ForgotPasswordForm v-if="showForm === 'forgot'"/>
@@ -23,7 +23,37 @@
 </script>
 
 <style scoped>
+    .loginContent {
+        background-color: var(--main-bg-color);
+    }
     .borders {
         max-width: 1000px;
+    }
+
+    /* Tablet */
+    @media (max-width: 1024px) {
+        .loginContent {
+            background-color:yellow;/* toRemove() */
+            width: 100vw;
+            height: 100vh;
+            border: none;
+            border-radius: 0px;
+            position: absolute;
+            top: 0;
+            margin: 0;
+        }
+    }
+    /* Phone */
+    @media (max-width: 768px) {
+        .loginContent {
+            background-color:blue;/* toRemove() */
+            width: 100vw;
+            height: 100vh;
+            border: none;
+            border-radius: 0px;
+            position: absolute;
+            top: 0;
+            margin: 0;
+        }
     }
 </style>
