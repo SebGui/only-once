222a7d923ea107ab2e1123945643d22559588c71
Sebastien <sebastien.guibourdin@gmail.com>
2024-10-16 13:49:45 +0200

    Register page update Mobile/Tablet/Desktop

--- src/components/auth/RegisterForm.vue
+++ src/components/auth/RegisterForm.vue
@@ -1,5 +1,10 @@
 <template>
-    <div>
+    <div class="registerContainer">
+        <!-- Mobile & tab icon -->
+        <div class="registerIcon">
+            <i class="material-icons">app_registration</i>
+         </div>
+
         <form @submit.prevent="handleRegister" id="registerForm">
             <input type="text" placeholder="Enter your login..." v-model="login" required autofocus>
             <input type="password" placeholder="Enter your password..." v-model="password" required>
@@ -9,13 +14,24 @@
             <span v-if="authStore.errorText != null" class="errorText">{{ authStore.errorText }}</span>
             <span v-if="authStore.successText != null" class="successText">{{ authStore.successText }}</span>
 
-            <button>Submit</button>
+            <button>
+                <i class="material-icons">app_registration</i>
+                Submit
+            </button>
         </form>
 
         <div class="switchAuth">
-            <span @click="handleSwitch('login')">Login</span> /
-            <span @click="handleSwitch('forgot')">Forgot password</span>
+            <button @click="handleSwitch('login')">
+                <i class="material-icons">login</i>
+                Login
+            </button>
+
+            <button @click="handleSwitch('forgot')">
+                <i class="material-icons">psychology_alt</i>
+                Forgot password
+            </button>
         </div>
+
     </div>
 </template>
 
@@ -76,23 +92,116 @@
     }
     input {
         margin: 10px 0;
+        height: 30px;
+        padding-left:10px;
     }
     button {
         margin-top:20px;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        transition: var(--transition-time);
+    }
+    #registerForm button:hover, #registerForm button:active {
+        background-color:white;
+        color: var(--main-bg-color)
     }
     .switchAuth {
         margin-bottom:10px;
-    }
-    .switchAuth span {
-        cursor:pointer;
-    }
-    .switchAuth span:hover {
-        color: var(--main-text-color-hover);
     }
     .errorText {
         color: var(--main-error-color);
     }
     .successText {
         color: var(--main-success-color);
+    }
+
+    .registerIcon {
+        display:none;
+        margin: 0px auto;
+    }
+    .registerContainer {background-color: var(--main-bg-color);}
+
+    .switchAuth i, #registerForm i {
+        margin-right:10px;
+        font-size:40px;
+    }
+    #registerForm button {
+        color:white;
+        background-color: var(--main-bg-color-darken);
+    }
+    
+    /* Desktop */
+    @media (min-width: 1024px) {
+        .switchAuth {
+            display: flex;
+            flex-direction: row;
+            justify-content: center;
+        }
+        .switchAuth button {
+            margin:10px;
+        }
+        .registerContent {
+           margin: 30vh auto;
+        }
+        #registerForm button {
+           margin-top:20px;
+           margin-bottom:20px;
+        }
+    }
+
+    /* Tablet */
+    @media (max-width: 1024px) {
+        .registerContent {}
+        .registerIcon {
+            color:white;
+            display:block;
+        }
+        .registerIcon .material-icons {
+            font-size: 120px;
+        }
+        .registerContainer {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            height: 100%;
+            width: 100vw;
+        }
+        #registerForm {
+            width: 100%;
+        }
+        #registerForm input {
+            height:40px;
+            font-size: 16px;
+            padding-left: 20px;
+        }
+        #registerForm button {
+            width:50%;
+            height:50px;
+            font-size: 16px;
+            margin-top: 70px;
+            margin: 70px auto 20px auto;
+        }
+        .switchAuth {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            width:100%;
+        }
+        .switchAuth button {
+            width:50%;
+            height:50px;
+            font-size: 16px;
+        }
+    }
+    /* Phone */
+    @media (max-width: 768px) {
+        .registerContent {}
+        .registerIcon {
+            display:block;
+        }
     }
 </style>
